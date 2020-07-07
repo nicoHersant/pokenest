1b157462996682e5ce352de4030457d668f0553f
NicoH <nicolas.hersant@artwai.com>
2020-07-07 10:28:03 +0200

    update boxes tests

--- pokenest/src/boxes/boxes.controller.spec.ts
+++ pokenest/src/boxes/boxes.controller.spec.ts
@@ -1,18 +1,17 @@
 
-import { Test } from '@nestjs/testing';
 import { BoxesController } from './boxes.controller';
 import { BoxesService } from './boxes.service';
 import { PokemonsService } from '../pokemons/pokemons.service';
-import { Model } from 'mongoose';
-import { Box } from '../schemas/box.schema';
-import { Pokemon } from '../schemas/pokemon.schema';
+//import { Model } from 'mongoose';
+//import { Box } from '../schemas/box.schema';
+//import { Pokemon } from '../schemas/pokemon.schema';
 
 describe('BoxesController', () => {
     let boxesController: BoxesController;
     let boxesService: BoxesService;
-    let boxModel: Model < Box >;
-    let pokemonModel: Model<Pokemon>;
     let pokemonService: PokemonsService;
+    //let boxModel: Model < Box >;
+    //let pokemonModel: Model<Pokemon>;
 
     beforeEach(() => {
         pokemonService = { findAll: jest.fn() } as any
@@ -23,10 +22,16 @@ describe('BoxesController', () => {
     describe('findAll', () => {
         it('should return an array of boxes', async () => {
             const result = ['test'];
-            ( boxesService.findAll as any).mockResolveValue(result)
-            jest.spyOn(boxesService, 'findAll').mockImplementation(() => result);
-
+            (boxesService.findAll as any).mockResolvedValue(result)
             expect(await boxesController.findAll()).toBe(result);
         });
     });
+ /*   describe('findOne', () => {
+        it('should return a boxe', async () => {
+            const result = ['test'];
+            (boxesService.findOne as any).mockResolveValue(result)
+            expect(await boxesController.findOne('5eff37917ecb9c7a422f0801')).toBe(result);
+        });
+    });
+*/
 });
