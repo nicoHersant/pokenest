c2996d4ad2c3ae604b71c0c48e7e79dddeff0cde
NicoH <nicolas.hersant@artwai.com>
2020-07-08 15:59:48 +0200

    add pokemon list

---
+++ pokefront/src/components/PokemonList.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './PokemonList.css'
+
+const PokemonList = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [next, setNext] = useState(undefined);
+  const [previous, setPrevious] = useState(undefined);
+  const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon')
+
+  useEffect(() => {
+    axios.get(url).then(({ data: { results, next, previous } }) => {
+      setPokemons(results)
+      setNext(next)
+      setPrevious(previous)
+    })
+  }, [url])
+
+  return (
+    <div>
+      <ul className="pkmnList">
+        {pokemons.map(pkmn => (
+          <li key={pkmn.name}>{pkmn.name}</li>
+        ))}
+      </ul>
+      <div style={{ display: 'inline' }}>
+        <button disabled={!previous} onClick={() => setUrl(previous)}>⬅️ Previous</button>
+        <button disabled={!next} onClick={() => setUrl(next)}>Next ➡️</button>
+      </div>
+    </div>
+
+  );
+}
+export default PokemonList;
