d61cc0e8ddb3b4f5960a899a1ef43890a34b962d
Sarah <sarah.drapeau@hotmail.fr>
2020-07-10 12:28:57 +0200

    begin to add function remove/create to pokemon front

--- pokefront/src/components/Pokemon.jsx
+++ pokefront/src/components/Pokemon.jsx
@@ -5,13 +5,22 @@ const Pokemon = (props) => {
     const [pokemon, setPokemon] = useState({});
     const [pokemonUrl, setPokemonUrl] = useState("http://localhost:3030/pokemons/");
 
+    const deletePokemon = (id) => {
+        axios.delete("http://localhost:3030/pokemons/"+id).then((response) => {
+            console.log(response);
+        }).catch( (error)=> {
+            console.log(error);
+        });
+    };
+
     useEffect(() => {
         axios.get("http://localhost:3030/pokemons/"+props.pokemonId).then((response) => {
                 setPokemon(response.data);
         }).catch( (error)=> {
                 console.log(error);
             });
-        }, [pokemonUrl, props]);
+        }, [pokemonUrl, props, deletePokemon]);
+
 
     return pokemon ? (
         <div className="content-detail-pokemon">
@@ -23,8 +32,13 @@ const Pokemon = (props) => {
                 ))}
             </ul>
 
+            <div className="actions">
+                <button className="actions-update">Update</button>
+                <button onClick={() => deletePokemon(pokemon._id)} className="actions-delete">Delete</button>
+            </div>
+
         </div>
-    ) : "Loading";
+    ) : "";
 };
 
 export default Pokemon;
--- pokefront/src/components/PokemonList.jsx
+++ pokefront/src/components/PokemonList.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const PokemonList = (props) => {
     const [pokemons, setPokemons] = useState([]);
+    const [creating, setIsCreating] = useState(false);
     const [url, setUrl] = useState('http://localhost:3030/pokemons');
 
     useEffect(() => {
@@ -15,7 +16,21 @@ const PokemonList = (props) => {
     }, [url]);
 
     return (
-        <div>
+        <div className="content-list-container">
+            { creating ?
+                <div className="form-creating">
+                    <input type="text"/>
+                    <input type="text"/>
+
+                    <div className="form-creating-actions">
+                        <button className="form-creating-actions-save" onClick={() => ""}>Save</button>
+                        <button className="form-creating-actions-cancel" onClick={() => setIsCreating(false)}>Cancel</button>
+                    </div>
+
+                </div>
+                : <button className="actions-create" onClick={() => setIsCreating(true)}>Create a new pokemon</button>
+            }
+
             <ul className="content-list-pokemon">
                 {pokemons.map(pkmn => (
                     <li key={pkmn._id} data-url={pkmn.name}
